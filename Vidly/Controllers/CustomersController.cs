43d486cb7a1a72f8797cc30fee7cb11237d1c765
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-11-12 14:14:27 -0800

    Added eager loading to Customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -4,22 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
 	public class CustomersController : Controller
 	{
+		public ApplicationDbContext _context;
+
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
 		// GET: Customers
 		public ActionResult Index()
 		{
-			var customers = GetCustomers();
+			var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 			return View(customers);
 		}
 
 		// GET: Customers/Details/Id
 		public ActionResult Details(int id)
 		{
-			var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+			var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
 			if (customer == null)
 			{
@@ -27,16 +40,6 @@ namespace Vidly.Controllers
 			}
 
 			return View(customer);
-		}
-
-		private IEnumerable<Customer> GetCustomers()
-		{
-			var customer1 = new Customer() { Id = 1, Name = "John Smith" };
-			var customer2 = new Customer() { Id = 2, Name = "Mary Williams" };
-			var customers = new List<Customer>();
-			customers.Add(customer1);
-			customers.Add(customer2);
-			return customers;
 		}
 	}
 }
