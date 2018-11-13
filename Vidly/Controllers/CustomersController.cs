6cc5819dc1d519f0ffbef24f1c393c7ec5e2208f
valeriaoshiro <valeriaoshiro19@gmail.com>
2018-11-13 14:48:55 -0800

    Create form view and ViewModels folder

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -40,6 +41,17 @@ namespace Vidly.Controllers
 			}
 
 			return View(customer);
+		}
+
+		// GET: Customers/New
+		public ActionResult New()
+		{
+			var membershipTypes = _context.MembershipTypes.ToList();
+			var viewModel = new NewCustomerViewModel
+			{
+				MembershipTypes = membershipTypes
+			};
+			return View(viewModel);
 		}
 	}
 }
